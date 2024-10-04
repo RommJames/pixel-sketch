0ea936d5e72c9d6e6e0a770f49dbc354cbf3aba0
RommJames <rj.cuya010@gmail.com>
2024-10-04 08:58:07 +0800

    Buttons for color brushes functioning successfully

--- src/script.js
+++ src/script.js
@@ -6,12 +6,15 @@ const brushColorValueHTML = document.querySelector("#brush-color-value");
 const btnBrushRainbowHTML = document.querySelector("#btn-brush-rainbow");
 const eraserBtnHTML = document.querySelector("#eraser-btn");
 const clearBtnHTML = document.querySelector("#clear-btn");
-const allBtn = document.querySelectorAll("#btn");
+const allBtnToggle = document.querySelectorAll(".btn-toggle");
+const btnBrushColorLabel = document.querySelector("#brush-color-label");
 
 // primitive data
 let pixelSizeValue = 16
 let paintAction = "mouseover" // click or mouseover
 let brushColorValue = "black"
+let randomize = false
+let rainbowColors = ["#ff9f9f", "#ffcc9f", "#ffff9f", "#9fff9f", "#9f9fff", "#c9a0ff", "#ff9fcd"]
 
 // Pixel Sketch Pad Creation
 updateSketchPadArea(pixelSizeValue)
@@ -29,7 +32,17 @@ function updateSketchPadArea(pixelSize){
             makePixelPadRowHTML.appendChild(makePixelPadHTML);
             
             makePixelPadHTML.addEventListener(paintAction,(e)=>{
-                brushColor(makePixelPadHTML, brushColorValue)
+                let color
+
+                if(randomize){
+                    let randomIndexRainbow = Math.ceil(Math.random() * rainbowColors.length - 1);
+                    let getRainbowColor = rainbowColors[randomIndexRainbow];
+                    color = getRainbowColor
+                }else{
+                    color = brushColorValue
+                }
+                                
+                brushColor(makePixelPadHTML, color);
             })
     
         }
@@ -54,20 +67,45 @@ sizeValue.addEventListener("change", (e)=>{
 // Dynamic Brush Color
 brushColorValueHTML.addEventListener("change", (e)=>{
     let value = e.target.value;
-    
+
     brushColorValue = value
 })
 
 // Eraser Tool
 eraserBtnHTML.addEventListener("click", (e)=>{
-   
-    brushColorValue = "white"
 
+    brushColorValue = "white"        
+
+   eraserBtnHTML.classList.add("active-btn");
+   btnBrushColorLabel.classList.remove("active-btn");
+   btnBrushRainbowHTML.classList.remove("active-rainbow-btn");
+   randomize = false
 })
+
+// Brush Color
+btnBrushColorLabel.addEventListener("click", (e)=>{
+
+    brushColorValue = brushColorValueHTML.value
+
+   eraserBtnHTML.classList.remove("active-btn");
+   btnBrushColorLabel.classList.add("active-btn");
+   btnBrushRainbowHTML.classList.remove("active-rainbow-btn");
+    randomize = false
+})
+
+// Rainbow Brush Button
+btnBrushRainbowHTML.addEventListener("click", (e)=>{
+    eraserBtnHTML.classList.remove("active-btn");
+    btnBrushColorLabel.classList.remove("active-btn");
+    btnBrushRainbowHTML.classList.add("active-rainbow-btn");
+    randomize = true
+})
+
+// Clear Button
 
 clearBtnHTML.addEventListener("click", (e)=>{
 
-    updateSketchPadArea(pixelSizeValue)
+    updateSketchPadArea(pixelSizeValue)    
 
 })
 
@@ -83,10 +121,6 @@ function dynamicSlider(){
     sizeValue.style.background = `linear-gradient(to right, #c87fdd ${progress}%, white ${progress}%)`;
 }
 
-// Button Activation
 
 // testing debug area
-btnBrushRainbowHTML.addEventListener("click", (e)=>{
-    let value = e.target.value  
-})
 
